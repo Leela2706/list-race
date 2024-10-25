cfc2ce667eec581e5e446f51f4b8300a0c255ef0
leela2706 <saileelavardhan12@gmail.com>
2024-10-25 12:23:18 +0530

    Project List Race

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import HeaderTop from './Components/HeaderTop';
+import TopArea from './Components/TopArea';
+import Hero from './Components/Hero';
+import ListTopics from './Components/ListTopics';
+import Works from './Components/Works';
+import Explore from './Components/Explore';
+import Reviews from './Components/Reviews';
+import Statistics from './Components/Statistics';
+import Blog from './Components/Blog';
+import Subscription from './Components/Subscription';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <HeaderTop/>
+     <TopArea/>   
+     <Hero/>
+     <ListTopics/>
+     <Works/>
+     <Explore/>
+     <Reviews/>
+     <Statistics/>
+     <Blog/>
+     <Subscription/>
+     <Footer/>
     </div>
   );
 }
